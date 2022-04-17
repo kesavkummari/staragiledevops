7e4cc3b08f0200e44f112dba68662576fcff3b1d
devops-kesav <kesav.kummari@outlook.com>
2022-04-17 13:01:22 +0530

    Updated code

--- hclterraform/main.tf
+++ hclterraform/main.tf
@@ -3,15 +3,38 @@ provider "aws" {
   profile = "devops"
 }
 
-# EC2 Instance 
+data "aws_ami" "selected_ami" {
+  most_recent = true
+  owners      = ["156779398815"]
+  filter {
+    name = "name"
+    values = [
+      "webapplication-${var.cloudbinary_version}",
+    ]
+  }
+  filter {
+    name   = "owner-id"
+    values = ["${var.ami_aws_account_id}"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
 
+
+# EC2 Instance 
 resource "aws_instance" "web" {
-  ami                    = "ami-0e400db9332cffb63"
+  ami                    = "data.aws_ami.selected_ami.id"
   instance_type          = "t2.micro"
   key_name               = "aws-kesav"
   subnet_id              = "subnet-ae87c8f1"
   vpc_security_group_ids = ["sg-03cb43ecdf16cd940"]
-  user_data              = file("web.sh")
+  #user_data              = file("web.sh")
   tags = {
     "Name"        = "IAC-WebServer"
     "ProjectName" = "Cloud Binary"
---
+++ hclterraform/variable.tf
@@ -0,0 +1,7 @@
+variable "cloudbinary_version" {
+  default = "1.0.0"
+
+}
+variable "ami_aws_account_id" {
+  default = "156779398815"
+}
